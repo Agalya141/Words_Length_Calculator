b614238a46a705caefcad218a31c992e9b2d350a
Agalya141 <agal4312@gmail.com>
2026-08-29 18:22:16 +0530

    feat: add JS logic to calculate word count

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function calculate(){
+let sentence = document.getElementById("sentence");
+let result = document.getElementById("result")
+let value = sentence.value.trim()
+    if (value === ''){
+        result.innerHTML = "No Words are entered"
+    }
+    else{
+   let word = sentence.value.split(/[ ,.\-]+/)
+   result.innerHTML = word.length;
+    }
+}
